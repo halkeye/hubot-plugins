8285de3a54cdb99a0640d76245dc17e38e9e12a6
Gavin Mogan <gavin@gavinmogan.com>
2016-12-30 00:21:42 -0800

    actually load the files

--- index.js
+++ index.js
@@ -2,5 +2,5 @@ module.exports = function (robot) {
   return [
     require.resolve('hubot-phrases/src/scripts/hubot-phrases.js'),
     require.resolve('hubot-variables/src/scripts/hubot-variables.js')
-  ];
+  ].map(robot.loadFile);
 };
